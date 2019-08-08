1ed9c0e94262acd310423484c297dff904a52e23
kerreal <weareme99@gmail.com>
2019-08-08 02:53:39 +0000

    Added addPlave Form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,20 @@
 class PlacesController < ApplicationController
 
   def index
-    @places = Place.paginate(page: params[:page], per_page: 10)
+    @places = Place.paginate(:per_page => 10, :page => params[:page])
   end
+
+  def create
+    Place.create(place_params)
+    redirect_to root_path
+  end
+
+  def new
+    @place = Place.new
+  end
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
+  end
+
 end
